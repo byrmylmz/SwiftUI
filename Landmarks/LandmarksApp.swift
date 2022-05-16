d0b0c5dab6d72034b329344caf5812f70a8ac22b
bayram <68708205+byrmylmz@users.noreply.github.com>
2022-05-16 09:33:49 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Bayram Yilmaz on 16/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
